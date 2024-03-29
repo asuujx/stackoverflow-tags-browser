6ac87b474a6fbe0023e5410f206bde625824285b
assuy <43821373+assuy@users.noreply.github.com>
2024-03-29 22:53:04 +0100

    Add dark mode functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+         </ThemeProvider>
+      </QueryClientProvider>
+   </React.StrictMode>
+);
