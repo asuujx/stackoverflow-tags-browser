6d17187274e5035f8c9d879c139416bc5de4ff42
assuy <43821373+assuy@users.noreply.github.com>
2024-04-01 01:27:17 +0200

    Add light and dark backgrounds

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,14 +2,20 @@ import "@/index.css";
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+   parameters: {
+      backgrounds: {
+         values: [
+            { name: "light", value: "hsl(45 100% 99%)" },
+            { name: "dark", value: "hsl(0 0% 12%)" },
+         ],
       },
-    },
-  },
+      controls: {
+         matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/i,
+         },
+      },
+   },
 };
 
 export default preview;
