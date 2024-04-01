13eac873d9908061d75326581d020ab32e8d3e6d
assuy <43821373+assuy@users.noreply.github.com>
2024-04-02 01:29:28 +0200

    Add Card story

---
+++ src/components/ui/card.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const meta: Meta<typeof Card> = {
+   title: "shadcn/Card",
+   component: Card,
+   parameters: {
+      docs: {
+         description: {
+            component:
+               "This component displays a card with header and content.",
+         },
+      },
+      layout: "centered",
+   },
+   tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Default: Story = {
+   render: () => (
+      <Card>
+         <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+         </CardHeader>
+         <CardContent>
+            <p>Card Content</p>
+         </CardContent>
+      </Card>
+   ),
+};
