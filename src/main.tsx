ffbd927434449b8412e6ff712b00d9a3ca9ab2bb
assuy <43821373+assuy@users.noreply.github.com>
2024-04-03 01:13:01 +0200

    Update ThemeProvider path

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "./components/Darkmode/ThemeProvider.tsx";
 import "./index.css";
-import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 const queryClient = new QueryClient();
 
