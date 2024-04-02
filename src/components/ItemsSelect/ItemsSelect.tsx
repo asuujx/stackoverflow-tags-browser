7fab2afe6319d9fcbfae828810eb9cf54afa8e81
assuy <43821373+assuy@users.noreply.github.com>
2024-04-02 02:01:06 +0200

    Change ItemsSelect path

---
+++ src/components/ItemsSelect/ItemsSelect.tsx
@@ -0,0 +1,44 @@
+import {
+   Select,
+   SelectContent,
+   SelectGroup,
+   SelectItem,
+   SelectLabel,
+   SelectTrigger,
+   SelectValue
+} from "@/components/ui/select";
+
+interface ItemsSelectProps {
+   onItemsSelect: (selectedItems: number) => void;
+}
+
+export default function ItemsSelect({ onItemsSelect }: ItemsSelectProps) {
+   const handleValueChange = (value: string) => {
+      onItemsSelect(parseInt(value));
+   };
+
+   return (
+      <Select onValueChange={handleValueChange}>
+         <SelectTrigger className="w-32">
+            <SelectValue placeholder="Amount" />
+         </SelectTrigger>
+         <SelectContent>
+            <SelectGroup>
+               <SelectLabel>Amount</SelectLabel>
+               <SelectItem value="25" className="cursor-pointer">
+                  25
+               </SelectItem>
+               <SelectItem value="50" className="cursor-pointer">
+                  50
+               </SelectItem>
+               <SelectItem value="75" className="cursor-pointer">
+                  75
+               </SelectItem>
+               <SelectItem value="100" className="cursor-pointer">
+                  100
+               </SelectItem>
+            </SelectGroup>
+         </SelectContent>
+      </Select>
+   );
+}
