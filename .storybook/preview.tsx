4c308a6a5676db7d22a9b31724dcf9084bc64f31
assuy <43821373+assuy@users.noreply.github.com>
2024-04-03 17:34:03 +0200

    Update dark mode switcher functionality

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,26 +5,27 @@ import React from "react";
 
 import { ThemeProvider } from "../src/components/Darkmode/ThemeProvider";
 
-export const decorators = [
-   (Story, context) => {
-      const theme = context.globals.scheme || "light";
-      return (
-         <ThemeProvider defaultTheme={theme}>
-            <Story />
-         </ThemeProvider>
-      );
+const withThemeProvider = (Story, context) => {
+   const theme = context.globals.scheme || "light";
+   return (
+      <ThemeProvider defaultTheme={theme}>
+         <Story />
+      </ThemeProvider>
+   );
+};
+
+const withThemeClass = withThemeByClassName({
+   themes: {
+      light: "",
+      dark: "dark",
    },
-   withThemeByClassName({
-      themes: {
-         light: "",
-         dark: "dark",
-      },
-      defaultTheme: "light",
-   }),
-];
+   defaultTheme: "light",
+});
 
 const preview: Preview = {
+   decorators: [withThemeProvider, withThemeClass],
    parameters: {
+      backgrounds: { disable: true },
       controls: {
          matchers: {
             color: /(background|color)$/i,
